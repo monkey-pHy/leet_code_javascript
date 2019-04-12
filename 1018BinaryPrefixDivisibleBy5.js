d5456ac685b949523d6084b408ba3b60fa3e0418
Caroline Peng <caroline.peng@groupm.com>
2019-04-12 18:10:59 +0800

    2019年4月12日刷题

--- 1018BinaryPrefixDivisibleBy5.js
+++ 1018BinaryPrefixDivisibleBy5.js
@@ -3,9 +3,9 @@ var prefixesDivBy5 = function(A) {
     let num = 0;
     for(const d of A){
         num =(num*2+d)%5;
-        arr.push(num);
+        arr.push(!num);
     }
     return arr;
 };
-var array= [0,1,1];
+var array= [0,1,1,1,1,1];
 console.log(prefixesDivBy5(array));
