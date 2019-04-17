6cf75aa2f90313d834c0adee6a40c4de6cf5308f
Caroline Peng <caroline.peng@groupm.com>
2019-04-17 17:07:51 +0800

    2019年4月17日刷题

--- 38CountandSay.js
+++ 38CountandSay.js
@@ -1,18 +1,23 @@
 var countAndSay = function(n) {
-    if(n==1) return "1";
-    let counySay = "1";
-    for(let i=2;i<=n;i++){
-        let numCount =1;
+    
+    if(n === 1) return "1";
+    
+    let countSay = "1";
+    
+    for(let i = 2; i <= n; i++) {
+        let numCount = 1;
         let temp = "";
-        for(let j=0;j<countSay.length;j++){
-            if(countSay[i]==counySay[j+1]){
+        for(let j = 0; j < countSay.length; j++) {
+            
+            if(countSay[j] === countSay[j+1]) {
                 numCount++;
-            }else{
-                temp+=numCount.toString()+countSay[j];
-                numCount =1;
+            } else {
+                temp += numCount.toString() + countSay[j];
+                numCount = 1;
             }
         }
         countSay = temp;
     }
-    return counySay;
+    
+    return countSay;
 };
