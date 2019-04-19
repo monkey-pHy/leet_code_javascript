c040903f2c2c29ed1b3e5462278d3f58b2ed8c83
Caroline Peng <caroline.peng@groupm.com>
2019-04-19 17:54:04 +0800

    2019年4月19日刷题

---
+++ 415AddStrings.js
@@ -0,0 +1,6 @@
+var addStrings = function(num1, num2) {
+    return (parseInt(num1)+parseInt(num2)).toString();
+};
+var s1 = "0";
+var s2 ="0"
+console.log(addStrings(s1,s2));
---
+++ 58LengthOfLastWord.js
@@ -0,0 +1,8 @@
+var lengthOfLastWord = function(s) {
+    if(s.trim().length === 0) return 0;
+    if(s.length <= 1) return s.length;
+    let words = s.trim().split(" ");
+    return  words[words.length -1].length
+};
+var s = "Hello World";
+console.log(lengthOfLastWord(s));
